939d33a377a390e4851f14bc973fc29829131940
ibuzaru <ibuzarujapan@gmail.com>
2025-01-30 11:24:00 +0900

    uuuuu

--- mysite/settings.py
+++ mysite/settings.py
@@ -97,7 +97,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+        
+       'default': dj_database_url.config(conn_max_age=600, ssl_require=True) 
+        
     }
 }
 
